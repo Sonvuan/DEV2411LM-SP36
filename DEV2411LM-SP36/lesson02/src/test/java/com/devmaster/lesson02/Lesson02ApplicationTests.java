bfb52730bab78bdea3358c4d1c1e66b0f6c10f7a
Sonvuan <son9xhy12345@gmail.com>
2025-02-21 19:31:21 +0700

    new

---
+++ DEV2411LM-SP36/lesson02/src/test/java/com/devmaster/lesson02/Lesson02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaster.lesson02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
